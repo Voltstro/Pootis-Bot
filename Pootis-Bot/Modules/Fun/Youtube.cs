080ffa079d0b81c63e5a56d5c19577d6137a6e87
Voltstro <me@voltstro.dev>
2018-12-30 22:17:21 +1000

    Google, Youtube, Giphy no api check

--- Pootis-Bot/Modules/Fun/Youtube.cs
+++ Pootis-Bot/Modules/Fun/Youtube.cs
@@ -21,71 +21,85 @@ namespace Pootis_Bot.Modules.Fun
         [Alias("yt")]
         public async Task YoutubeSearch([Remainder] string search = "")
         {
-            if(search != "")
+            if (search != "")
             {
-                try
+                if (Config.bot.apiYoutubeKey.Trim() != "")
                 {
-                    var youtube = new YouTubeService(new BaseClientService.Initializer()
+                    try
                     {
-                        ApiKey = Config.bot.apiYoutubeKey,
-                        ApplicationName = this.GetType().ToString()
-                    });
-
-                    var searchListRequest = youtube.Search.List("snippet");
-                    searchListRequest.Q = search; // Replace with your search term.
-                    searchListRequest.MaxResults = 10;
-
-                    // Call the search.list method to retrieve results matching the specified query term.
-                    var searchListResponse = await searchListRequest.ExecuteAsync();
-
-                    List<string> videos = new List<string>();
-                    List<string> channels = new List<string>();
-
-                    // Add each result to the appropriate list, and then display the lists of
-                    // matching videos and channels.
-                    foreach (var searchResult in searchListResponse.Items)
-                    {
-                        switch (searchResult.Id.Kind)
+                        var youtube = new YouTubeService(new BaseClientService.Initializer()
                         {
-                            case "youtube#video":
-                                videos.Add(String.Format($"{searchResult.Snippet.Title}\n({ytstartLink + searchResult.Id.VideoId})"));
-                                break;
+                            ApiKey = Config.bot.apiYoutubeKey,
+                            ApplicationName = this.GetType().ToString()
+                        });
 
-                            case "youtube#channel":
-                                channels.Add(String.Format($"{searchResult.Snippet.Title}\n({ytChannelStart + searchResult.Id.ChannelId})"));
-                                break;
+                        var searchListRequest = youtube.Search.List("snippet");
+                        searchListRequest.Q = search; // Replace with your search term.
+                        searchListRequest.MaxResults = 10;
+
+                        // Call the search.list method to retrieve results matching the specified query term.
+                        var searchListResponse = await searchListRequest.ExecuteAsync();
+
+                        List<string> videos = new List<string>();
+                        List<string> channels = new List<string>();
+
+                        // Add each result to the appropriate list, and then display the lists of
+                        // matching videos and channels.
+                        foreach (var searchResult in searchListResponse.Items)
+                        {
+                            switch (searchResult.Id.Kind)
+                            {
+                                case "youtube#video":
+                                    videos.Add(String.Format($"{searchResult.Snippet.Title}\n({ytstartLink + searchResult.Id.VideoId})"));
+                                    break;
+
+                                case "youtube#channel":
+                                    channels.Add(String.Format($"{searchResult.Snippet.Title}\n({ytChannelStart + searchResult.Id.ChannelId})"));
+                                    break;
+                            }
                         }
+
+                        string _videos = String.Format("**Videos:**\n{0}\n", string.Join("\n", videos));
+                        string _channels = String.Format("**Channels:**\n{0}\n", string.Join("\n", channels));
+
+                        EmbedBuilder embed = new EmbedBuilder();
+                        EmbedFooterBuilder embedfoot = new EmbedFooterBuilder();
+                        embed.Title = $"Youtube Search '{search}'";
+                        embed.WithDescription($"{_videos} \n {_channels}");
+
+                        embedfoot.WithIconUrl(Context.User.GetAvatarUrl());
+                        embedfoot.WithText("Commanded issued by " + Context.User);
+
+                        embed.WithFooter(embedfoot);
+                        embed.WithColor(new Color(229, 57, 38));
+                        await Context.Channel.SendMessageAsync("", false, embed.Build());
                     }
+                    catch (Exception ex)
+                    {
+                        Global.ColorMessage($"[{Global.TimeNow()}] An error occured while user '{Context.User}' tryied searching '{search}' on Youtube. \nError Details: \n{ex.Message}", ConsoleColor.Red);
 
-                    string _videos = String.Format("**Videos:**\n{0}\n", string.Join("\n", videos));
-                    string _channels = String.Format("**Channels:**\n{0}\n", string.Join("\n", channels));
-
-                    EmbedBuilder embed = new EmbedBuilder();
-                    EmbedFooterBuilder embedfoot = new EmbedFooterBuilder();
-                    embed.Title = $"Youtube Search '{search}'";
-                    embed.WithDescription($"{_videos} \n {_channels}");
-
-                    embedfoot.WithIconUrl(Context.User.GetAvatarUrl());
-                    embedfoot.WithText("Commanded issued by " + Context.User); 
-
-                    embed.WithFooter(embedfoot);
-                    embed.WithColor(new Color(229, 57, 38));
-                    await Context.Channel.SendMessageAsync("", false, embed.Build());
+                        EmbedBuilder embed = new EmbedBuilder
+                        {
+                            Title = "Youtube Search Error"
+                        };
+                        embed.WithDescription($"An Error Occured. It is best to tell the owner of this bot this error.\n**Error Details: ** {ex.Message}");
+                        embed.WithColor(new Color(229, 57, 38));
+                        await Context.Channel.SendMessageAsync("", false, embed.Build());
+                        return;
+                    }
                 }
-                catch (Exception ex)
+                else
                 {
-                    Global.ColorMessage($"[{Global.TimeNow()}] An error occured while user '{Context.User}' tryied searching '{search}' on Youtube. \nError Details: \n{ex.Message}", ConsoleColor.Red);
-
                     EmbedBuilder embed = new EmbedBuilder
                     {
                         Title = "Youtube Search Error"
                     };
-                    embed.WithDescription($"An Error Occured. It is best to tell the owner of this bot this error.\n**Error Details: ** {ex.Message}");
+                    embed.WithDescription($"We are sorry, but youtube search is disabled by the bot owner.");
                     embed.WithColor(new Color(229, 57, 38));
                     await Context.Channel.SendMessageAsync("", false, embed.Build());
-                    return;
-                }         
-            }
+                }
+            } 
+
             else
             {
                 EmbedBuilder embed = new EmbedBuilder
