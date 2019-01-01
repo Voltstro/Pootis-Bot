ac9eb47245c71db5813e6245ed9bf1cb0e7f7031
Voltstro <me@voltstro.dev>
2019-01-02 01:14:00 +1000

    Added two bat scripts to make building easier.

---
+++ Build/buildWin10-x64_Debug.bat
@@ -0,0 +1,4 @@
+@echo off
+title Building for Windows 10-x64 Debug
+dotnet build ../Pootis-Bot.sln -c Debug -r win10-x64
+pause
---
+++ Build/buildWin10-x64_Release.bat
@@ -0,0 +1,4 @@
+@echo off
+title Building for Windows 10-x64 Release
+dotnet build ../Pootis-Bot.sln -c Release -r win10-x64
+pause
