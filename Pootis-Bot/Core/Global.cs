8d720bdee361018211b9e28973c7d393960e2fe7
Voltstro <me@voltstro.dev>
2019-01-02 23:21:02 +1000

    Removed unnessary using tags. Migrated Wording.cs code to be within Global.cs

--- Pootis-Bot/Core/Global.cs
+++ Pootis-Bot/Core/Global.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Pootis_Bot.Core
 {
@@ -16,6 +14,38 @@ namespace Pootis_Bot.Core
         public static string TimeNow()
         {
             return DateTime.Now.ToString("h:mm:ss tt");
+        }
+
+        public static string Title(string s)
+        {
+            char[] array = s.ToCharArray();
+            // Handle the first letter in the string.
+            if (array.Length >= 1)
+            {
+                if (char.IsLower(array[0]))
+                {
+                    array[0] = char.ToUpper(array[0]);
+                }
+            }
+            // Scan through the letters, checking for spaces.
+            // ... Uppercase the lowercase letters following spaces.
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (array[i - 1] == ' ')
+                {
+                    if (char.IsLower(array[i]))
+                    {
+                        array[i] = char.ToUpper(array[i]);
+                    }
+                }
+            }
+            return new string(array);
+        }
+
+        public static int RandomNumber(int min, int max)
+        {
+            Random random = new Random();
+            return random.Next(min, max);
         }
 
     }
--- Pootis-Bot/Modules/Basic.cs
+++ Pootis-Bot/Modules/Basic.cs
@@ -1,12 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using Discord;
 using Discord.Commands;
 using Discord.WebSocket;
-using Pootis_Bot.Core.ServerList;
 
 namespace Pootis_Bot.Modules
 {
