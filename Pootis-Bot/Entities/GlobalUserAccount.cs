31f55a789792ba992d2410a593637eaf3660b7e3
Voltstro <me@voltstro.dev>
2019-06-29 15:30:47 +1000

    Added JsonIgnore to UserAccount, clean some other parts up

--- Pootis-Bot/Entities/GlobalUserAccount.cs
+++ Pootis-Bot/Entities/GlobalUserAccount.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -12,6 +13,7 @@ namespace Pootis_Bot.Entities
 
         public string Msg { get; set; }
 
+        [JsonIgnore]
         public uint LevelNumber
         {
             get
