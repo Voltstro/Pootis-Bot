c1470b3fa528076b3376bef2212d09973d38aaeb
Liam <45032877+CreepysinMe@users.noreply.github.com>
2019-09-22 17:51:00 +1000

    Updated scripts with powershell versions

--- Build/Publish_Win-x86.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-title Building for Windows x86
-dotnet publish ../Pootis-Bot.sln -c Release -r win-x86
-echo
-echo Build complete @ Pootis-Bot\bin\Release\netcoreapp2.2\win-x86\publish
-pause
