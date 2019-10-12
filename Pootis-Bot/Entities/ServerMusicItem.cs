5265888b7d6da978a6e78dc490479ba44d780fca
Voltstro <me@voltstro.dev>
2019-10-13 00:06:28 +1000

    Added summaries to ServerMusicItem

--- Pootis-Bot/Entities/ServerMusicItem.cs
+++ Pootis-Bot/Entities/ServerMusicItem.cs
@@ -16,12 +16,34 @@ namespace Pootis_Bot.Entities
 		/// </summary>
 		public bool IsPlaying { get; set; }
 
+		/// <summary>
+		/// Are we ending the current song
+		/// </summary>
 		public bool IsExit { get; set; }
 
+		/// <summary>
+		/// Audio client
+		/// </summary>
 		public IAudioClient AudioClient { get; set; }
+
+		/// <summary>
+		/// Audio channel
+		/// </summary>
 		public SocketVoiceChannel AudioChannel { get; set; }
+
+		/// <summary>
+		/// The text message channel we started with
+		/// </summary>
 		public ISocketMessageChannel StartChannel { get; set; }
+
+		/// <summary>
+		/// Discord stream
+		/// </summary>
 		public AudioOutStream Discord { get; set; }
+
+		/// <summary>
+		/// FFMpeg process
+		/// </summary>
 		public Process FfMpeg { get; set; }
 	}
 }
