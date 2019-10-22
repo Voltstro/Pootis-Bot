d320dd111775234da0771e7007a7b4e2e0838503
Voltstro <me@voltstro.dev>
2019-10-22 21:32:54 +1000

    Added `wiki` command

---
+++ Pootis-Bot/Modules/Fun/WikipediaSearch.cs
@@ -0,0 +1,46 @@
+﻿using System.Text;
+using System.Threading.Tasks;
+using CreepysinStudios.WikiDotNet;
+using Discord;
+using Discord.Commands;
+
+namespace Pootis_Bot.Modules.Fun
+{
+	public class WikipediaSearch : ModuleBase<SocketCommandContext>
+	{
+		// Module Information
+		// Original Author  - Creepysin
+		// Description      - Wikipedia search
+		// Contributors     - Creepysin, 
+
+		//NOTICE ON WIKI.NET ==========
+		//WIKI.NET IS STILL IN-DEVELOPMENT BY ETERNALCLICKBAIT AND ME(Creepysin)
+		//YOU CAN GET WIKI.NET AT https://github.com/Creepysin-Studios/Wiki.Net
+
+		[Command("wiki")]
+		[Alias("wikipedia")]
+		[Summary("Searches Wikipedia")]
+		public async Task Wikipedia(string search = "")
+		{
+			if (string.IsNullOrWhiteSpace(search))
+			{
+				await Context.Channel.SendMessageAsync("A search needs to be entered! E.G: `wiki C#`");
+				return;
+			}
+
+			EmbedBuilder embed = new EmbedBuilder();
+			StringBuilder sb = new StringBuilder();
+			embed.WithTitle("Wikipedia Search");
+
+			WikiSearchResponse response = WikiSearcher.Search(search);
+			for (int i = 0; i < 5; i++)
+			{
+				WikiSearchResult result = response.SearchResults[i];
+				sb.Append($"[{result.Title}]({result.Url})\n{result.Preview}\n");
+			}
+
+			embed.WithDescription(sb.ToString());
+			await Context.Channel.SendMessageAsync("", false, embed.Build());
+		}
+	}
+}
