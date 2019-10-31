3c38479d3b52e5df77240e5aaf605fd8ec68d254
Voltstro <me@voltstro.dev>
2019-10-31 21:33:04 +1000

    Updated Wiki.Net
    You can have a limit on how many articles you want to search now.

--- Pootis-Bot/Modules/Fun/WikipediaSearch.cs
+++ Pootis-Bot/Modules/Fun/WikipediaSearch.cs
@@ -3,6 +3,7 @@ using System.Threading.Tasks;
 using CreepysinStudios.WikiDotNet;
 using Discord;
 using Discord.Commands;
+using Pootis_Bot.Core;
 
 namespace Pootis_Bot.Modules.Fun
 {
@@ -13,9 +14,9 @@ namespace Pootis_Bot.Modules.Fun
 		// Description      - Wikipedia search
 		// Contributors     - Creepysin, 
 
-		//NOTICE ON WIKI.NET ==========
-		//WIKI.NET IS STILL IN-DEVELOPMENT BY ETERNALCLICKBAIT AND ME(Creepysin)
-		//YOU CAN GET WIKI.NET AT https://github.com/Creepysin-Studios/Wiki.Net
+		//This use a library called Wiki.Net
+		//It was developed by my good friend EternalClickbait and partially by me(Creepysin).
+		//You can get it here: https://github.com/Creepysin-Studios/Wiki.Net
 
 		[Command("wiki")]
 		[Alias("wikipedia")]
@@ -28,24 +29,60 @@ namespace Pootis_Bot.Modules.Fun
 				return;
 			}
 
+			await Context.Channel.SendMessageAsync("", false, WikiSearch(search));
+		}
+
+		[Command("wiki")]
+		[Alias("wikipedia")]
+		[Summary("Searches Wikipedia")]
+		public async Task Wikipedia(int maxSearchResults = 15, [Remainder] string search = "")
+		{
+			if (string.IsNullOrWhiteSpace(search))
+			{
+				await Context.Channel.SendMessageAsync("A search needs to be entered! E.G: `wiki C#`");
+				return;
+			}
+
+			await Context.Channel.SendMessageAsync("", false, WikiSearch(search, maxSearchResults));
+		}
+
+		private Embed WikiSearch(string search, int maxSearch = 10)
+		{
 			EmbedBuilder embed = new EmbedBuilder();
+
 			StringBuilder sb = new StringBuilder();
 			embed.WithTitle("Wikipedia Search");
 			embed.WithColor(FunCmdsConfig.wikipediaSearchColor);
 
-			//TODO: Once max search responses are added into Wiki.NET, add max search responses
-			WikiSearchResponse response = WikiSearcher.Search(search);
-			for (int i = 0; i < 5; i++)
+			WikiSearchResponse response = WikiSearcher.Search(search, new WikiSearchSettings
 			{
-				WikiSearchResult result = response.SearchResults[i];
-				sb.Append($"[{result.Title}]({result.Url})\n{result.Preview}\n");
+				ResultLimit = maxSearch
+			});
+
+			foreach (WikiSearchResult result in response.Query.SearchResults)
+			{
+				string message =
+					$"**[{result.Title}]({result.ConstantUrl})** (Words: {result.WordCount})\n{result.Preview}\n\n";
+
+				//There is a character limit of 2048, so lets make sure we don't hit that
+				if (sb.Length >= 2048)
+				{
+					continue;
+				}
+
+				if (sb.Length + message.Length >= 2048)
+				{
+					continue;
+				}
+
+				sb.Append(message);
 			}
 
 			embed.WithDescription(sb.ToString());
 			embed.WithFooter($"Search by {Context.User.Username}", Context.User.GetAvatarUrl());
 			embed.WithCurrentTimestamp();
 
-			await Context.Channel.SendMessageAsync("", false, embed.Build());
+			return embed.Build();
 		}
 	}
 }
