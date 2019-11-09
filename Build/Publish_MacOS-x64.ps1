706f6a039c8fa32e24517b7e1d061118835bc61c
Voltstro <me@voltstro.dev>
2019-11-09 22:13:14 +1000

    Updated publish scripts for .NET Core 3.0

--- Build/Publish_MacOS-x64.ps1
+++ Build/Publish_MacOS-x64.ps1
@@ -1,5 +1,5 @@
-dotnet publish ../Pootis-Bot.sln -c Release -r osx-x64
-Remove-Item -Path "..\Pootis-Bot\bin\Release\netcoreapp2.2\osx-x64\publish\Pootis-Bot.pdb"
-Copy-Item "..\LICENSE.md" -Destination "..\Pootis-Bot\Bin\Release\netcoreapp2.2\osx-x64\publish\license.txt"
-Compress-Archive -Path "..\Pootis-Bot\bin\Release\netcoreapp2.2\osx-x64\publish\*" -DestinationPath "..\Pootis-Bot\bin\Release\netcoreapp2.2\osx-x64.zip" -CompressionLevel Optimal -Force
+dotnet publish ../Pootis-Bot.sln -c Release -r osx-x64 --framework netcoreapp3.0
+Remove-Item -Path "..\Pootis-Bot\bin\Release\netcoreapp3.0\osx-x64\publish\Pootis-Bot.pdb"
+Copy-Item "..\LICENSE.md" -Destination "..\Pootis-Bot\Bin\Release\netcoreapp3.0\osx-x64\publish\license.txt"
+Compress-Archive -Path "..\Pootis-Bot\bin\Release\netcoreapp3.0\osx-x64\publish\*" -DestinationPath "..\Pootis-Bot\bin\Release\netcoreapp2.2\osx-x64.zip" -CompressionLevel Optimal -Force
 pause
--- Build/Publish_Win-x64.ps1
+++ Build/Publish_Win-x64.ps1
@@ -1,5 +1,5 @@
-dotnet publish ../Pootis-Bot.sln -c Release -r win-x64
-Remove-Item -Path "..\Pootis-Bot\bin\Release\netcoreapp2.2\win-x64\publish\Pootis-Bot.pdb"
-Copy-Item "..\LICENSE.md" -Destination "..\Pootis-Bot\Bin\Release\netcoreapp2.2\win-x64\publish\license.txt"
-Compress-Archive -Path "..\Pootis-Bot\bin\Release\netcoreapp2.2\win-x64\publish\*" -DestinationPath "..\Pootis-Bot\bin\Release\netcoreapp2.2\win-x64.zip" -CompressionLevel Optimal -Force
+dotnet publish ../Pootis-Bot.sln -c Release -r win-x64 --framework netcoreapp3.0
+Remove-Item -Path "..\Pootis-Bot\bin\Release\netcoreapp3.0\win-x64\publish\Pootis-Bot.pdb"
+Copy-Item "..\LICENSE.md" -Destination "..\Pootis-Bot\Bin\Release\netcoreapp3.0\win-x64\publish\license.txt"
+Compress-Archive -Path "..\Pootis-Bot\bin\Release\netcoreapp3.0\win-x64\publish\*" -DestinationPath "..\Pootis-Bot\bin\Release\netcoreapp2.2\win-x64.zip" -CompressionLevel Optimal -Force
 pause
