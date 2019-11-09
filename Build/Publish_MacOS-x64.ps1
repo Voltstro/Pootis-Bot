4fddeecd3c2ad1abd0068a2eb791efc06adb3895
Voltstro <me@voltstro.dev>
2019-11-09 21:54:37 +1000

    Updated to .net core 3.1

--- Build/Publish_MacOS-x64.ps1
+++ Build/Publish_MacOS-x64.ps1
@@ -1,4 +1,4 @@
-dotnet publish ../Pootis-Bot.sln -c Release -r osx-x64 --framework netcoreapp2.2
+dotnet publish ../Pootis-Bot.sln -c Release -r osx-x64
 Remove-Item -Path "..\Pootis-Bot\bin\Release\netcoreapp2.2\osx-x64\publish\Pootis-Bot.pdb"
 Copy-Item "..\LICENSE.md" -Destination "..\Pootis-Bot\Bin\Release\netcoreapp2.2\osx-x64\publish\license.txt"
 Compress-Archive -Path "..\Pootis-Bot\bin\Release\netcoreapp2.2\osx-x64\publish\*" -DestinationPath "..\Pootis-Bot\bin\Release\netcoreapp2.2\osx-x64.zip" -CompressionLevel Optimal -Force
