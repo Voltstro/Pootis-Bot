26c3066b2dad87e197aaa0cb69398952707acc81
Voltstro <me@voltstro.dev>
2019-12-12 23:55:40 +1000

    Update ConfigAudio.cs

--- src/Pootis-Bot/Structs/Config/ConfigAudio.cs
+++ src/Pootis-Bot/Structs/Config/ConfigAudio.cs
@@ -4,9 +4,19 @@ namespace Pootis_Bot.Structs.Config
 {
 	public struct ConfigAudio
 	{
+		/// <summary>
+		/// Are the audio services enabled
+		/// </summary>
 		public bool AudioServicesEnabled { get; set; }
+
+		/// <summary>
+		/// Should we log when a song starts or stops on a guild
+		/// </summary>
 		public bool LogPlayStopSongToConsole { get; set; }
 
+		/// <summary>
+		/// Max video time
+		/// </summary>
 		public TimeSpan MaxVideoTime { get; set; }
 	}
 }
