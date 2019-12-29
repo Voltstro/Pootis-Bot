f05e41b37e461b5d5602ed13eedfa96c9903b150
Voltstro <me@voltstro.dev>
2019-12-29 22:40:56 +1000

    Updated publish scripts

--- publish-scripts/Publish_Linux-x64.ps1
+++ publish-scripts/Publish_Linux-x64.ps1
@@ -1,5 +1,5 @@
 dotnet publish ../Pootis-Bot.sln -c Release -r linux-x64 --framework netcoreapp3.0
 Remove-Item -Path "..\src\Pootis-Bot\bin\Release\netcoreapp3.0\linux-x64\publish\Pootis-Bot.pdb"
 Copy-Item "..\LICENSE.md" -Destination "..\src\Pootis-Bot\Bin\Release\netcoreapp3.0\linux-x64\publish\license.txt"
-Compress-Archive -Path "..\src\Pootis-Bot\bin\Release\netcoreapp3.0\linux-x64\publish\*" -DestinationPath "..\src\Pootis-Bot\bin\Release\netcoreapp3.0\linux-x64.zip" -CompressionLevel Optimal -Force
+Compress-Archive -Path "..\src\Pootis-Bot\bin\Release\netcoreapp3.0\linux-x64\publish\*" -DestinationPath "..\src\Pootis-Bot\bin\Release\pootis-bot-linux-x64.zip" -CompressionLevel Optimal -Force
 pause
--- publish-scripts/Publish_Win-x64.ps1
+++ publish-scripts/Publish_Win-x64.ps1
@@ -1,5 +1,5 @@
 dotnet publish ../Pootis-Bot.sln -c Release -r win-x64 --framework netcoreapp3.0
 Remove-Item -Path "..\src\Pootis-Bot\bin\Release\netcoreapp3.0\win-x64\publish\Pootis-Bot.pdb"
 Copy-Item "..\LICENSE.md" -Destination "..\src\Pootis-Bot\Bin\Release\netcoreapp3.0\win-x64\publish\license.txt"
-Compress-Archive -Path "..\src\Pootis-Bot\bin\Release\netcoreapp3.0\win-x64\publish\*" -DestinationPath "..\src\Pootis-Bot\bin\Release\netcoreapp3.0\win-x64.zip" -CompressionLevel Optimal -Force
+Compress-Archive -Path "..\src\Pootis-Bot\bin\Release\netcoreapp3.0\win-x64\publish\*" -DestinationPath "..\src\Pootis-Bot\bin\Release\pootis-bot-win-x64.zip" -CompressionLevel Optimal -Force
 pause
