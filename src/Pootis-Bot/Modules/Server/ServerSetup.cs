0d1bad2e8748b3c18648e35b4ab2760007b3f43a
Voltstro <me@voltstro.dev>
2020-01-13 00:02:27 +1000

    Moved setup commands related to point roles to its own module

--- src/Pootis-Bot/Modules/Server/ServerSetup.cs
+++ src/Pootis-Bot/Modules/Server/ServerSetup.cs
@@ -1,13 +1,10 @@
-﻿using System.Text;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Discord;
 using Discord.Commands;
 using Discord.WebSocket;
 using Pootis_Bot.Core.Managers;
 using Pootis_Bot.Entities;
 using Pootis_Bot.Helpers;
-using Pootis_Bot.Preconditions;
-using Pootis_Bot.Structs.Server;
 
 namespace Pootis_Bot.Modules.Server
 {
@@ -18,83 +15,7 @@ namespace Pootis_Bot.Modules.Server
 		// Description      - Helps the server owner set up the bot for use
 		// Contributors     - Creepysin, 
 
-		[Command("addrolepoints")]
-		[Alias("addpointsrole", "add points role", "add role points")]
-		[Summary("Adds a role to give after a user gets a certain amount of points")]
-		[RequireGuildOwner]
-		public async Task AddRolePoints(uint pointsAmount, string roleName)
-		{
-			SocketRole role = RoleUtils.GetGuildRole(Context.Guild, roleName);
-			if (role == null)
-			{
-				await Context.Channel.SendMessageAsync("That role doesn't exists!");
-				return;
-			}
-
-			ServerList server = ServerListsManager.GetServer(Context.Guild);
-
-			if (pointsAmount >= server.PointGiveAmount)
-			{
-				await Context.Channel.SendMessageAsync(
-					$"Sorry, but you have to set the points require higher then {server.PointGiveAmount}");
-
-				return;
-			}
-
-			ServerRolePoints serverRolePoints = new ServerRolePoints
-			{
-				RoleId = role.Id,
-				PointsRequired = pointsAmount
-			};
-
-			server.ServerRolePoints.Add(serverRolePoints);
-			ServerListsManager.SaveServerList();
-
-			await Context.Channel.SendMessageAsync(
-				$"Ok, when a user gets {pointsAmount} points they shell receive the **{role.Name}** role.\nPlease note any user who already have {pointsAmount} points won't get the role.");
-		}
-
-		[Command("removerolepoints")]
-		[Alias("removepointsrole", "remove points role", "remove role points")]
-		[Summary("Removes a role to give to a user after they get a certain amount of points")]
-		[RequireGuildOwner]
-		public async Task RemoveRolePoints(uint pointsAmount)
-		{
-			ServerList server = ServerListsManager.GetServer(Context.Guild);
-			ServerRolePoints serverRolePoints = server.GetServerRolePoints(pointsAmount);
-			if (serverRolePoints.PointsRequired == 0)
-			{
-				await Context.Channel.SendMessageAsync(
-					$"There are no role points that have {pointsAmount} points as there requirement!");
-				return;
-			}
-
-			server.ServerRolePoints.Remove(serverRolePoints);
-			ServerListsManager.SaveServerList();
-
-			//TODO: Maybe a better reply?
-			await Context.Channel.SendMessageAsync(
-				$"The {serverRolePoints.PointsRequired} points required one was removed.");
-		}
-
-		[Command("rolepoints")]
-		[Alias("pointsrole", "role points", "points role")]
-		[Summary("Gets all the role points")]
-		[RequireGuildOwner]
-		public async Task RolePoints()
-		{
-			StringBuilder sb = new StringBuilder();
-			ServerList server = ServerListsManager.GetServer(Context.Guild);
-
-			sb.Append("__**Server Role Points**__\n");
-
-			foreach (ServerRolePoints rolePoint in server.ServerRolePoints)
-				sb.Append(
-					$"[{RoleUtils.GetGuildRole(Context.Guild, rolePoint.RoleId)}] **{rolePoint.PointsRequired}**\n");
-
-			await Context.Channel.SendMessageAsync(sb.ToString());
-		}
-
+		//TODO: We are gonna rename theses as 'Opt Roles'. So as in they are roles you opt into.
 		[Command("rolegiveadd")]
 		[Alias("role give add", "add role give")]
 		[Summary("Assigns you a specified role if the user meets a requirement")]
@@ -166,5 +87,7 @@ namespace Pootis_Bot.Modules.Server
 
 			await Context.Channel.SendMessageAsync($"Removed role give '{roleGiveName}'.'");
 		}
+
+		//TODO: Add the ability to change the server points amount
 	}
 }
