4bb64098331a6dfb055400f447f746f26845204e
Voltstro <me@voltstro.dev>
2020-02-01 00:52:16 +1000

    Update GiphyData.cs

--- src/Pootis-Bot/Structs/Giphy/GiphyData.cs
+++ src/Pootis-Bot/Structs/Giphy/GiphyData.cs
@@ -1,10 +1,28 @@
 ﻿namespace Pootis_Bot.Structs.Giphy
 {
+	/// <summary>
+	/// Giphy search result data
+	/// </summary>
 	public struct GiphyData
 	{
+		/// <summary>
+		/// The URL to the original image
+		/// </summary>
 		public string GifUrl { get; set; }
+
+		/// <summary>
+		/// The title of the gif
+		/// </summary>
 		public string GifTitle { get; set; }
+
+		/// <summary>
+		/// The author of the gif
+		/// </summary>
 		public string GifAuthor { get; set; }
+
+		/// <summary>
+		/// The link to the Giphy post
+		/// </summary>
 		public string GifLink { get; set; }
 	}
 }
