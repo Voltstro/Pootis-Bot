3127a62bf8fb343b94cf40d3324100d97b9b1807
Voltstro <me@voltstro.dev>
2020-02-01 00:54:38 +1000

    Update GiphySearchResults.cs

--- src/Pootis-Bot/Structs/Giphy/GiphySearchResults.cs
+++ src/Pootis-Bot/Structs/Giphy/GiphySearchResults.cs
@@ -2,14 +2,32 @@
 {
 	public enum ErrorReason
 	{
+		/// <summary>
+		/// No API key provided
+		/// </summary>
 		NoApiKey,
+
+		/// <summary>
+		/// A general error
+		/// </summary>
 		Error
 	}
 
 	public struct GiphySearchResult
 	{
+		/// <summary>
+		/// Was the search successful?
+		/// </summary>
 		public bool IsSuccessful { get; set; }
+
+		/// <summary>
+		/// If the search wasn't successful, why?
+		/// </summary>
 		public ErrorReason ErrorReason { get; set; }
+
+		/// <summary>
+		/// The search result, only if <see cref="IsSuccessful"/> is true.
+		/// </summary>
 		public GiphyData Data { get; set; }
 	}
 }
