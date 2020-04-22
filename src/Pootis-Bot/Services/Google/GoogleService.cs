ca87af6b458c012d6ec373de81e0487e11f8859d
Voltstro <me@voltstro.dev>
2020-04-22 21:43:45 +1000

    Minor clean-up of GoogleService.cs

--- src/Pootis-Bot/Services/Google/GoogleService.cs
+++ src/Pootis-Bot/Services/Google/GoogleService.cs
@@ -25,8 +25,7 @@ namespace Pootis_Bot.Services.Google
 				using (CustomsearchService google = new CustomsearchService(new BaseClientService.Initializer
 				{
 					ApiKey = Config.bot.Apis.ApiGoogleSearchKey,
-					ApplicationName = appName,
-					
+					ApplicationName = appName
 				}))
 				{
 					CseResource.ListRequest searchListRequest = google.Cse.List();
