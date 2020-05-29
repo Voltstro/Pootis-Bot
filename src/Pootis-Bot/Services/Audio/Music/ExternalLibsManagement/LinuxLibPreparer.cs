a88538a421491fb84b4a4ea0168a25900b80ad17
Voltstro <me@voltstro.dev>
2020-05-29 21:22:38 +1000

    Update LinuxLibPreparer.cs

--- src/Pootis-Bot/Services/Audio/Music/ExternalLibsManagement/LinuxLibPreparer.cs
+++ src/Pootis-Bot/Services/Audio/Music/ExternalLibsManagement/LinuxLibPreparer.cs
@@ -1,4 +1,6 @@
 #if LINUX
+
+using System;
 using System.Diagnostics;
 using System.IO;
 using System.IO.Compression;
@@ -16,36 +18,52 @@ namespace Pootis_Bot.Services.Audio.Music.ExternalLibsManagement
             string externalDirectory = Config.bot.AudioSettings.ExternalDirectory;
 
             //If either ffmpeg, opus or libsodium doesn't exist, we need to download them
-            return File.Exists($"{externalDirectory}ffmpeg") && File.Exists("opus.dll") && File.Exists("libsodium.dll");
+            return File.Exists($"{externalDirectory}ffmpeg") && File.Exists("opus.so") && File.Exists("libsodium.so");
         }
 
         public void DownloadFiles(AudioExternalLibFiles libsUrls)
         {
-            Logger.Log("Downloading files for Linux...");
+            try
+            {
+                Logger.Log("Downloading files for Linux...");
 
-            //Download all audio service files for Linux
-            Logger.Log($"Downloading ffmpeg from {libsUrls.FfmpegDownloadUrl}");
-            WebUtils.DownloadFileAsync(libsUrls.FfmpegDownloadUrl, "Temp/ffmpeg.zip").GetAwaiter().GetResult();
-            Logger.Log($"Downloading needed DLLs from {libsUrls.LibsDownloadUrl}");
-            WebUtils.DownloadFileAsync(libsUrls.LibsDownloadUrl, "Temp/dlls.zip").GetAwaiter().GetResult();
+                //Download all audio service files for Linux
+                Logger.Log($"Downloading ffmpeg from {libsUrls.FfmpegDownloadUrl}");
+                WebUtils.DownloadFileAsync(libsUrls.FfmpegDownloadUrl, "Temp/ffmpeg.zip").GetAwaiter().GetResult();
+                Logger.Log($"Downloading needed DLLs from {libsUrls.LibsDownloadUrl}");
+                WebUtils.DownloadFileAsync(libsUrls.LibsDownloadUrl, "Temp/dlls.zip").GetAwaiter().GetResult();
 
-            //Extract required files
-            Logger.Log("Extracting files...");
-            ZipFile.ExtractToDirectory("Temp/dlls.zip", "./", true);
-            ZipFile.ExtractToDirectory("Temp/ffmpeg.zip", "Temp/ffmpeg/", true);
+                //Extract required files
+                Logger.Log("Extracting files...");
+                ZipFile.ExtractToDirectory("Temp/dlls.zip", "./", true);
+                ZipFile.ExtractToDirectory("Temp/ffmpeg.zip", "Temp/ffmpeg/", true);
 
-            //Copy the needed parts of ffmpeg to the right directory
-            Logger.Log("Setting up ffmpeg");
-            Global.DirectoryCopy("Temp/ffmpeg/ffmpeg-linux-64/", "External/", true);
+                //Copy the needed parts of ffmpeg to the right directory
+                Logger.Log("Setting up ffmpeg");
+                Global.DirectoryCopy("Temp/ffmpeg/ffmpeg-linux-64/", "External/", true);
 			
-            //Because linux, we need the right permissions
-            ChmodFile("External/ffmpeg", "700");
+                //Because linux, we need the right permissions
+                ChmodFile("External/ffmpeg", "700");
 
-            //Delete unnecessary files
-            Logger.Log("Cleaning up...");
-            File.Delete("Temp/dlls.zip");
-            File.Delete("Temp/ffmpeg.zip");
-            Directory.Delete("Temp/ffmpeg", true);
+                //Delete unnecessary files
+                Logger.Log("Cleaning up...");
+                File.Delete("Temp/dlls.zip");
+                File.Delete("Temp/ffmpeg.zip");
+                Directory.Delete("Temp/ffmpeg", true);
+            }
+            catch (Exception ex)
+            {
+#if DEBUG
+                Logger.Log(
+                    $"An error occured while preparing music services: {ex}\nMusic services has now been disabled!",
+                    LogVerbosity.Error);
+#else
+				Logger.Log($"An error occured while preparing music services: {ex.Message}\nMusic services has now been disabled!", LogVerbosity.Error);
+#endif
+
+                Config.bot.AudioSettings.AudioServicesEnabled = false;
+                Config.SaveConfig();
+            }
         }
         
         private static void ChmodFile(string file, string flag)
