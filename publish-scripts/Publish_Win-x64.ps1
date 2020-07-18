112d594639303185040e94e861abacf4236b3464
Voltstro <me@voltstro.dev>
2020-07-18 13:30:43 +1000

    Updated scripts to use .NET Core 3.1
    Published builds will no longer be self contained

--- publish-scripts/Publish_Win-x64.ps1
+++ publish-scripts/Publish_Win-x64.ps1
@@ -1,5 +1,4 @@
-dotnet publish ../Pootis-Bot.sln -c Release -r win-x64 --framework netcoreapp3.0
-Remove-Item -Path "..\src\Pootis-Bot\bin\Release\netcoreapp3.0\win-x64\publish\Pootis-Bot.pdb"
-Copy-Item "..\LICENSE.md" -Destination "..\src\Pootis-Bot\Bin\Release\netcoreapp3.0\win-x64\publish\license.txt"
-Compress-Archive -Path "..\src\Pootis-Bot\bin\Release\netcoreapp3.0\win-x64\publish\*" -DestinationPath "..\src\Pootis-Bot\bin\Release\pootis-bot-win-x64.zip" -CompressionLevel Optimal -Force
+dotnet publish ../Pootis-Bot.sln -c Release -r win-x64 --framework netcoreapp3.1 --self-contained false
+Copy-Item "..\LICENSE.md" -Destination "..\src\Pootis-Bot\Bin\Release\netcoreapp3.1\win-x64\publish\license.txt"
+Compress-Archive -Path "..\src\Pootis-Bot\bin\Release\netcoreapp3.1\win-x64\publish\*" -DestinationPath "..\src\Pootis-Bot\bin\Release\pootis-bot-win-x64.zip" -CompressionLevel Optimal -Force
 pause
