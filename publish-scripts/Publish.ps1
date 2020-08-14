86661a3a995e57631b0f602c27bd3e66342a37f9
Voltstro <me@voltstro.dev>
2020-08-14 22:36:08 +1000

    Update Publish.ps1

--- publish-scripts/Publish.ps1
+++ publish-scripts/Publish.ps1
@@ -8,4 +8,4 @@ $framework = "netcoreapp3.1"
 
 dotnet publish ../Pootis-Bot.sln -c Release -r $rid --framework $framework --self-contained false --nologo
 Copy-Item "..\LICENSE.md" -Destination ("..\src\Pootis-Bot\Bin\Release\" + $framework + "\" + $rid + "\publish\license.txt")
-Compress-Archive -Path ("..\src\Pootis-Bot\Bin\Release\" + $framework + "\" + $rid + "\publish\*") -DestinationPath "..\src\Pootis-Bot\bin\Release\pootis-bot-win-x64.zip" -CompressionLevel Optimal -Force
+Compress-Archive -Path ("..\src\Pootis-Bot\Bin\Release\" + $framework + "\" + $rid + "\publish\*") -DestinationPath ("..\src\Pootis-Bot\bin\Release\pootis-bot-" + $rid + ".zip") -CompressionLevel Optimal -Force
