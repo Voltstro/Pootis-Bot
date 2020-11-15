d8c3acc639014ae45f721a720ec492c2274602f9
Voltstro <me@voltstro.dev>
2020-11-15 19:49:27 +1000

    Added logger

---
+++ src/Pootis-Bot.Core/Core/Bot.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Pootis_Bot.Core.Logging;
+using Pootis_Bot.Exceptions;
+
+namespace Pootis_Bot.Core
+{
+	/// <summary>
+	///     Main class for handling the bot
+	/// </summary>
+	public class Bot : IDisposable
+	{
+		public bool IsRunning { get; private set; }
+
+		/// <summary>
+		///     Disposes of this bot instance
+		/// </summary>
+		public void Dispose()
+		{
+			//The bot has already shutdown
+			if (!IsRunning)
+				throw new InitializationException("The bot has already shutdown!");
+
+			ReleaseResources();
+			GC.SuppressFinalize(this);
+		}
+
+		/// <summary>
+		///     Runs the bot
+		/// </summary>
+		public void Run()
+		{
+			//Don't want to be-able to run the bot multiple times
+			if (IsRunning)
+				throw new InitializationException("A bot is already running!");
+
+			IsRunning = true;
+
+			//Init the logger
+			Logger.Init();
+			Logger.Info("Starting bot...");
+		}
+
+		~Bot()
+		{
+			ReleaseResources();
+		}
+
+		private void ReleaseResources()
+		{
+			Logger.Shutdown();
+
+			IsRunning = false;
+		}
+	}
+}
--- src/Pootis-Bot/Program.cs
+++ src/Pootis-Bot/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using Pootis_Bot.Core;
 using Pootis_Bot.PackageDownloader;
 
 namespace Pootis_Bot
@@ -8,26 +9,12 @@ namespace Pootis_Bot
 	{
 		public static void Main(string[] args)
 		{
-			NuGetPackageResolver resolver = new NuGetPackageResolver("net5.0");
-			Console.WriteLine("Discord.Net");
-			List<string> discordResult = resolver.DownloadPackage("Discord.Net", new Version(2, 2, 0)).GetAwaiter().GetResult();
-
-			foreach (string dll in discordResult)
-			{
-				Console.WriteLine(dll);
-			}
-
-			Console.WriteLine("Wiki.Net");
-			List<string> wikiNetResult = resolver.DownloadPackage("Wiki.Net", new Version(3, 0, 0)).GetAwaiter().GetResult();
-
-			foreach (string dll in wikiNetResult)
-			{
-				Console.WriteLine(dll);
-			}
-
-			resolver.Dispose();
+			Bot bot = new Bot();
+			bot.Run();
 
 			Console.ReadKey();
+
+			bot.Dispose();
 		}
 	}
 }
