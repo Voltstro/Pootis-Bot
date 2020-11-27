8d17afe28e725c37187cdf38744c6963abcf29b9
Voltstro <me@voltstro.dev>
2020-11-27 17:32:20 +1000

    Added VersionUtils

--- src/Modules/Pootis-Bot.Module.Basic/BasicCommands.cs
+++ src/Modules/Pootis-Bot.Module.Basic/BasicCommands.cs
@@ -1,11 +1,9 @@
-﻿using System;
-using System.Linq;
-using System.Reflection;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Discord;
 using Discord.Commands;
 using Pootis_Bot.Config;
 using Pootis_Bot.Core;
+using Pootis_Bot.Helper;
 
 namespace Pootis_Bot.Module.Basic
 {
@@ -26,29 +24,10 @@ namespace Pootis_Bot.Module.Basic
 			EmbedBuilder embed = new EmbedBuilder();
 			embed.WithTitle("Hello!");
 			embed.WithDescription($"Hello! My name is {displayName}!");
-			embed.WithFooter($"Pootis-Bot: v{GetAppVersion()} - Discord.Net: v{GetDiscordNetVersion()}");
+			embed.WithFooter($"Pootis-Bot: v{VersionUtils.GetApplicationVersion()} - Discord.Net: v{VersionUtils.GetDiscordNetVersion()}");
 			embed.WithColor(new Color(241, 196, 15));
 
 			await Context.Channel.SendMessageAsync("", false, embed.Build());
-		}
-
-		private static string GetAppVersion()
-		{
-			Assembly assembly = Assembly.GetEntryAssembly();
-			if (assembly == null) return null;
-
-			return assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>()
-				?.InformationalVersion;
-		}
-
-		public static string GetDiscordNetVersion()
-		{
-			Assembly[] assemblies = AppDomain.CurrentDomain.GetAssemblies();
-
-			return (from assembly in assemblies
-					where assembly.GetName().Name == "Discord.Net.Core"
-					select assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion)
-				.FirstOrDefault();
 		}
 	}
 }
---
+++ src/Pootis-Bot.Core/Helper/VersionUtils.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace Pootis_Bot.Helper
+{
+	/// <summary>
+	///		Utils for getting version numbers
+	/// </summary>
+	public static class VersionUtils
+	{
+		/// <summary>
+		///		Gets the version of the application
+		/// </summary>
+		/// <returns></returns>
+		public static Version GetApplicationVersion()
+		{
+			Assembly assembly = Assembly.GetEntryAssembly();
+			return assembly == null ? null : new Version(assembly?.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion ?? string.Empty);
+		}
+
+		/// <summary>
+		///		Gets the version of Discord.Net
+		///		<para>Please note that Discord.Net needs to be loaded before this will work</para>
+		/// </summary>
+		/// <returns></returns>
+		/// <exception cref="NullReferenceException"></exception>
+		public static Version GetDiscordNetVersion()
+		{
+			Assembly[] assemblies = AppDomain.CurrentDomain.GetAssemblies();
+
+			Assembly discordCore = assemblies.FirstOrDefault(x => x.GetName().Name == "Discord.Net.Core");
+			if(discordCore == null)
+				throw new NullReferenceException("Discord.Net.Core doesn't appear to be loaded!");
+
+			return new Version(discordCore?.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion ?? string.Empty);
+		}
+	}
+}
