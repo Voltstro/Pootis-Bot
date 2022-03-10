5e23effda8779afe7234a463d91f5470d5a0e7de
Voltstro <me@voltstro.dev>
2022-03-10 23:20:24 +1000

    Reminders module uses interactions now

--- src/Modules/Pootis-Bot.Module.Reminders/RemindersCommands.cs
+++ src/Modules/Pootis-Bot.Module.Reminders/RemindersCommands.cs
@@ -1,24 +1,27 @@
 ﻿using System;
 using System.Threading.Tasks;
-using Discord.Commands;
+using Discord;
+using Discord.Interactions;
 
 namespace Pootis_Bot.Module.Reminders
 {
-    [Group("remind")]
-    [Summary("Commands for reminders")]
-    public class RemindersCommands : ModuleBase<SocketCommandContext>
+    [Group("remind", "Commands for reminders")]
+    public class RemindersCommands : InteractionModuleBase<SocketInteractionContext>
     {
-        [Command("me")]
-        [Summary("Sets a reminder for you")]
-        public async Task RemindMe(TimeSpan time, [Remainder] string message)
+        [SlashCommand("me", "Sets a reminder for you")]
+        public async Task RemindMe(TimeSpan time, string message)
         {
+            await RespondAsync("Creating reminder...");
+            
             //Get end time
             DateTime startTime = DateTime.UtcNow;
             DateTime endTime = startTime.Add(time);
 
-            RemindersService.CreateAndStartReminder(Context.User, message, Context.Message, Context.Guild, startTime, endTime, Context.Client);
-            await Context.Channel.SendMessageAsync(
-                $"Your reminder was set, I will remind you at {startTime:hh:mm:ss tt} UTC.");
+            IUserMessage response = await GetOriginalResponseAsync();
+
+            RemindersService.CreateAndStartReminder(Context.User, message, response, Context.Guild, startTime, endTime, Context.Client);
+            
+            await response.ModifyAsync(x => x.Content = $"Your reminder was set, I will remind you at {startTime:hh:mm:ss tt} UTC.");
         }
     }
 }
