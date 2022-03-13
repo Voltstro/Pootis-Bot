17ec90d42b1498a25c5ee9fcc7b3d33fbc2b8512
Voltstro <me@voltstro.dev>
2022-03-14 00:16:20 +1000

    Start of fun module

---
+++ src/Modules/Pootis-Bot.Module.Fun/FunInteractions.cs
@@ -0,0 +1,50 @@
+using System.Threading.Tasks;
+using Discord;
+using Discord.Interactions;
+using WikiDotNet;
+
+namespace Pootis_Bot.Module.Fun;
+
+[Group("", "Fun related commands")]
+public class FunInteractions : InteractionModuleBase<SocketInteractionContext>
+{
+    [SlashCommand("wiki", "Searches wikipedia")]
+    public async Task WikiSearch(string search)
+    {
+        if (string.IsNullOrWhiteSpace(search))
+        {
+            await RespondAsync("Search cannot be empty or white space!");
+            return;
+        }
+
+        await RespondAsync("Searching...");
+        
+        WikiSearchResponse searchResult = WikiSearcher.Search(search, new WikiSearchSettings
+        {
+            ResultLimit = 8
+        });
+        if (!searchResult.WasSuccessful)
+        {
+            //TODO: We should read the errors
+            await RespondAsync("Wiki search was not successful!");
+            return;
+        }
+
+        EmbedBuilder embedBuilder = new();
+        embedBuilder.WithTitle($"Wikipedia Search Results for `{search}`");
+        foreach (WikiSearchResult querySearchResult in searchResult.Query.SearchResults)
+        {
+            embedBuilder.AddField($"{querySearchResult.Title} - ({querySearchResult.ConstantUrl("en")})",
+                $"{querySearchResult.Preview}...");
+        }
+
+        embedBuilder.WithTimestamp(searchResult.Timestamp);
+
+        IUserMessage responseAsync = await GetOriginalResponseAsync();
+        await responseAsync.ModifyAsync(x =>
+        {
+            x.Content = string.Empty;
+            x.Embed = embedBuilder.Build();
+        });
+    }
+}
---
+++ src/Modules/Pootis-Bot.Module.Fun/FunModule.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+using Discord.WebSocket;
+using Pootis_Bot.Helper;
+using Pootis_Bot.Modules;
+
+namespace Pootis_Bot.Module.Fun;
+
+public sealed class FunModule : Modules.Module
+{
+    protected override ModuleInfo GetModuleInfo()
+    {
+        return new ModuleInfo("FunModule", "Voltstro", new Version(VersionUtils.GetCallingVersion()), new ModuleDependency("Wiki.Net", new Version(3, 1, 0), "Wiki.Net.dll"));
+    }
+
+    protected override Task ClientConnected(DiscordSocketClient client)
+    {
+        return Task.CompletedTask;
+    }
+}
