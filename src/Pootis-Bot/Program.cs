2df0a745bf8643b4e9da04810acce340a7f92972
Voltstro <me@voltstro.dev>
2022-03-21 21:15:45 +1000

    Use figlet text
    * Use new style entry point

--- src/Pootis-Bot/Program.cs
+++ src/Pootis-Bot/Program.cs
@@ -1,26 +1,16 @@
-﻿using Pootis_Bot.Core;
+﻿using Pootis_Bot;
+using Pootis_Bot.Core;
 using Pootis_Bot.Helper;
+using Spectre.Console;
 
-namespace Pootis_Bot;
+//Ascii art of Pootis-Bot because why not ¯\_(ツ)_/¯
+FigletFont font = FigletFont.Parse(Resources.StandardFont);
+AnsiConsole.Write(new FigletText(font, "Pootis-Bot"));
+AnsiConsole.MarkupLine($"        [bold]Version[/]: {VersionUtils.GetApplicationVersion()}");
+AnsiConsole.Write("\n");
 
-public static class Program
-{
-    public static void Main(string[] args)
-    {
-        //Ascii art of Pootis-Bot because why not ¯\_(ツ)_/¯
-        System.Console.WriteLine(@"__________              __  .__                 __________        __   ");
-        System.Console.WriteLine(@"\______   \____   _____/  |_|__| ______         \______   \ _____/  |_ ");
-        System.Console.WriteLine(@" |     ___/  _ \ /  _ \   __\  |/  ___/  ______  |    |  _//  _ \   __\");
-        System.Console.WriteLine(@" |    |  (  <_> |  <_> )  | |  |\___ \  /_____/  |    |   (  <_> )  |  ");
-        System.Console.WriteLine(@" |____|   \____/ \____/|__| |__/____  >          |______  /\____/|__|  ");
-        System.Console.WriteLine(@"                                    \/                  \/             ");
-        System.Console.WriteLine($"			Version: {VersionUtils.GetApplicationVersion()}");
-        System.Console.WriteLine();
+Bot bot = new();
+bot.Run().GetAwaiter().GetResult();
+Bot.ConsoleLoop();
 
-        Bot bot = new();
-        bot.Run().GetAwaiter().GetResult();
-        Bot.ConsoleLoop();
-
-        bot.Dispose();
-    }
-}
+bot.Dispose();
