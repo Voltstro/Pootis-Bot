c7cb63bbc4e45ea164e8544c58b6e99d4dbea99c
Voltstro <me@voltstro.dev>
2025-10-28 22:02:12 +1000

    Update help module to be only /help with full command list Improve DM sending Fix caching

--- src/Pootis-Bot/Modules/HelpModule.cs
+++ src/Pootis-Bot/Modules/HelpModule.cs
@@ -5,136 +5,214 @@ using System.Text;
 using System.Threading.Tasks;
 using Discord;
 using Discord.Interactions;
-using Discord.Net;
-using Pootis_Bot.Core;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Logging;
+using Pootis_Bot.Shared.Helper;
 
 namespace Pootis_Bot.Modules;
 
 /// <summary>
 ///     Module that provides the help command
 /// </summary>
-[Group("help", "Provides help commands")]
 public class HelpModule : InteractionModuleBase<SocketInteractionContext>
 {
-    private readonly InteractionService interactionService;
-    private string[]? cachedHelpMenu;
+    private const string HelpMenuKey = "HelpMenu";
+    private static readonly Type[] AllowSlashCommandParameterTypes =
+    [
+        typeof(string),
+        typeof(int),
+        typeof(float),
+        typeof(double),
+        typeof(decimal),
+        typeof(bool)
+    ];
 
-    public HelpModule(InteractionService cmdService)
+    private readonly ILogger<HelpModule> logger;
+    private readonly IMemoryCache memoryCache;
+    private readonly InteractionService interactionService;
+    
+    public HelpModule(ILogger<HelpModule> logger, IMemoryCache memoryCache, InteractionService cmdService)
     {
+        this.logger = logger;
+        this.memoryCache = memoryCache;
         interactionService = cmdService;
     }
 
-    [SlashCommand("get", "Gets help on commands")]
-    public async Task Help(string? command = null)
+    [SlashCommand("help", "Gets help on all commands")]
+    public async Task Help()
     {
-        if (string.IsNullOrWhiteSpace(command))
+        string[]? messagesToSend;
+        try
+        {
+            messagesToSend = memoryCache.GetOrCreate(HelpMenuKey, entry =>
+            {
+                //Get help groups
+                HelpCommandModule[] helpGroups = GetHelpMenu();
+
+                //Discord has a message limit of 2000 chars
+                //So split everything out into as few groups as possible
+                List<string> messages =
+                [
+                    string.Empty
+                ];
+
+                int messageIndex = 0;
+                int messageCharCount = 0;
+                foreach (HelpCommandModule helpGroup in helpGroups)
+                {
+                    string formattedHelpGroup = FormatHelpGroup(helpGroup);
+                    //Check if we will go over the char limit, if so add a new message and reset
+                    if (formattedHelpGroup.Length + messageCharCount > 2000)
+                    {
+                        messages.Add(string.Empty);
+                        messageIndex++;
+                        messageCharCount = 0;
+                    }
+
+                    messages[messageIndex] += formattedHelpGroup;
+                    messageCharCount += formattedHelpGroup.Length;
+                }
+                
+                return messages.ToArray();
+            });
+
+            if (messagesToSend == null)
+                throw new NullReferenceException();
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, "An error occured while getting help menu!");
+            await RespondAsync(Messages.CreatedFailed("help menu"));
+            return;
+        }
+
+        //Now send the messages
+        IMessageChannel channel = Context.Channel;
+        if (Context.Guild != null)
         {
             await RespondAsync("I will DM you the help info!");
-
-            DmChat dmChat = new(Context.User);
-
-            foreach (string message in GetHelpMenu())
-                try
-                {
-                    await dmChat.SendMessage(message);
-                }
-                catch (HttpException)
-                {
-                    await Context.Channel.SendMessageAsync(
-                        "Sorry, but I can't seem to send you a DM for some reason, you might have your account set to not allow DMs from users.");
-                }
-                catch (Exception)
-                {
-                    await Context.Channel.SendMessageAsync(
-                        "Sorry, but I can't seem to send you a DM for some reason, this might be an issue with Discord.");
-                }
+            channel = await Context.User.CreateDMChannelAsync();
         }
         else
         {
-            command = command.ToLowerInvariant();
+            await RespondAsync("Here is help with Pootis-Bot's commands!");
+        }
 
-            SlashCommandInfo[] searchResult = interactionService.SlashCommands.Where(x => x.Name == command).ToArray();
-            if (searchResult.Length == 0)
-            {
-                await RespondAsync("No results where found!");
-                return;
-            }
-
-            EmbedBuilder embed = new();
-            embed.WithTitle($"Help for `{command}`");
-
-            foreach (SlashCommandInfo commandInfo in searchResult)
-                embed.AddField(commandInfo.Name,
-                    $"**Summary**: {commandInfo.Description}\n**Usage**: {BuildCommandUsage(commandInfo)}");
-
-            await RespondAsync(embed: embed.Build());
+        foreach (string message in messagesToSend)
+        {
+            await channel.SendMessageAsync(message);
         }
     }
 
-    private IEnumerable<string> GetHelpMenu()
+    private HelpCommandModule[] GetHelpMenu()
     {
-        if (cachedHelpMenu != null)
-            return cachedHelpMenu;
-
-        List<string> groups = new();
+        List<HelpCommandModule> helpGroups = new();
         ModuleInfo[] modules = interactionService.Modules.ToArray();
         foreach (ModuleInfo module in modules)
         {
-            string message = $"```diff\n+ {module.Name}\n  - Summary: {module.Description}\n";
-            message = module.SlashCommands.Aggregate(message,
-                (current, command) =>
-                    current +
-                    $"\n- {BuildCommandFormat(command)}\n  - Summary: {command.Description}\n  - Usage: {BuildCommandUsage(command)}");
-            message += "\n```";
+            //Exclude this module
+            if (module.Name == nameof(HelpModule))
+                continue;
+            
+            //Exclude module with no commands
+            IReadOnlyList<SlashCommandInfo> slashCommands = module.SlashCommands;
+            if (slashCommands.Count < 1)
+                continue;
+            
+            HelpCommand[] helpItems = new HelpCommand[slashCommands.Count];
+            for (int i = 0; i < helpItems.Length; i++)
+            {
+                SlashCommandInfo slashCommand = slashCommands[i];
+                HelpCommandParameter[] parameters = new HelpCommandParameter[slashCommand.Parameters.Count];
+                for (int j = 0; j < parameters.Length; j++)
+                {
+                    SlashCommandParameterInfo slashCommandParameter = slashCommand.Parameters[j];
+                    string? defaultValue = null;
+                    if (slashCommandParameter.DefaultValue != null && AllowSlashCommandParameterTypes.Any(t => t == slashCommandParameter.DefaultValue.GetType()))
+                        defaultValue = slashCommandParameter.DefaultValue.ToString();
+                    
+                    parameters[j] =
+                        new HelpCommandParameter(slashCommandParameter.Name, defaultValue);
+                }
+                
+                helpItems[i] = new HelpCommand(slashCommand.Name, slashCommand.Description, parameters);
+            }
 
-            //If its the first group, ignore
-            if (groups.Count != 0)
+            string? slashGroupName = null;
+            if(!string.IsNullOrWhiteSpace(module.SlashGroupName))
+                slashGroupName = module.SlashGroupName;
+
+            string? parentModuleName = null;
+            string? parentSlashGroupName = null;
+            ModuleInfo? parentModule = module.Parent;
+            if (parentModule != null)
             {
-                //Get the combined message size of the last group and this group
-                int lastMessageAndNewMessageLength = groups[^1].Length + message.Length;
-                if (lastMessageAndNewMessageLength < 1998)
-                    groups[^1] += message;
-                else //Too big, send as its own
-                    groups.Add(message);
+                parentModuleName  = parentModule.Name;
+                
+                if (!string.IsNullOrWhiteSpace(parentModule.SlashGroupName))
+                    parentSlashGroupName = parentModule.SlashGroupName;
             }
-            else
-            {
-                groups.Add(message);
-            }
+            
+            HelpCommandModule helpCommandModule = new(module.Name, slashGroupName, parentModuleName, parentSlashGroupName, module.Description, helpItems);
+            helpGroups.Add(helpCommandModule);
         }
-
-        cachedHelpMenu = groups.ToArray();
-        return cachedHelpMenu;
+        
+        return helpGroups.ToArray();
     }
 
-    private string BuildCommandUsage(SlashCommandInfo command)
+    private string FormatHelpGroup(HelpCommandModule helpCommandModule)
     {
-        StringBuilder commandUsage = new StringBuilder();
-        commandUsage.Append($"`{BuildCommandFormat(command)}");
-        foreach (SlashCommandParameterInfo parameter in command.Parameters)
+        StringBuilder sb = new();
+        sb.AppendLine("```diff");
+        sb.Append("+ ");
+        if (helpCommandModule.ParentModuleName != null)
+            sb.Append($"{helpCommandModule.ParentModuleName} / ");
+        
+        sb.AppendLine(helpCommandModule.Name);
+        sb.AppendLine($"  - Summary: {helpCommandModule.Description}");
+        sb.AppendLine();
+
+        foreach (HelpCommand helpCommand in helpCommandModule.Commands)
         {
-            commandUsage.Append($" <{parameter.Name.ToLower()}");
-            if (parameter.DefaultValue != null) commandUsage.Append($" = {parameter.DefaultValue}");
-
-            commandUsage.Append(">");
+            sb.Append("- ");
+            if (helpCommandModule.GroupName != null)
+                sb.Append($"{helpCommandModule.GroupName} ");
+            sb.AppendLine(helpCommand.Name);
+            sb.AppendLine($"  - Summary: {helpCommand.Description}");
+            
+            sb.Append("  - Usage: `/");
+            if (helpCommandModule.ParentGroupName != null)
+                sb.Append($"{helpCommandModule.ParentGroupName} ");
+            if (helpCommandModule.GroupName != null)
+                sb.Append($"{helpCommandModule.GroupName} ");
+            sb.Append(helpCommand.Name);
+            foreach (HelpCommandParameter parameter in helpCommand.Parameters)
+            {
+                sb.Append(" <");
+                sb.Append(parameter.Name);
+                if (parameter.DefaultValue != null)
+                    sb.Append($" = {parameter.DefaultValue}");
+                sb.Append(">");
+            }
+            
+            sb.AppendLine("`");
         }
-
-        commandUsage.Append("`");
-        return commandUsage.ToString();
+        
+        sb.Append("```");
+        return sb.ToString();
     }
 
-    private string BuildCommandFormat(SlashCommandInfo command)
-    {
-        string groupName = command.Module.SlashGroupName;
-        string commandName = command.Name.ToLower();
+    /// <summary>
+    ///     Details of a module and its commands
+    /// </summary>
+    /// <param name="Name">The name of the module</param>
+    /// <param name="GroupName">The name of the group</param>
+    /// <param name="ParentModuleName">The name of the parent module</param>
+    /// <param name="Description">Description of the module</param>
+    /// <param name="Commands">Commands in the module</param>
+    private record HelpCommandModule(string Name, string? GroupName, string? ParentModuleName, string? ParentGroupName, string Description, HelpCommand[] Commands);
+    
+    private record HelpCommand(string Name, string Description, HelpCommandParameter[] Parameters);
 
-        string commandFormat = commandName;
-        if (string.IsNullOrEmpty(groupName))
-            return commandFormat;
-
-        groupName = groupName.ToLower();
-        if (groupName != commandName)
-            commandFormat = $"{groupName} {commandName}";
-        return commandFormat;
-    }
+    private record HelpCommandParameter(string Name, string? DefaultValue);
 }
